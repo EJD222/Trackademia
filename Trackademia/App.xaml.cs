62a35a1159b5f3c1b0552eee8c38ba0cb5b6b79e
JoshDaguio <bennethpineda@gmail.com>
2024-12-03 23:12:08 +0800

    Add project files

---
+++ Trackademia/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Trackademia
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
