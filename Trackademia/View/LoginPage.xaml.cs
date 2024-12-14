898a347fe76cd08822354263d7c8b73cca0a5ca9
EJD222 <eljdiaz500@gmail.com>
2024-12-14 23:11:16 +0800

    Adjust button widths

--- Trackademia/View/LoginPage.xaml.cs
+++ Trackademia/View/LoginPage.xaml.cs
@@ -10,7 +10,6 @@ public partial class LoginPage : ContentPage
     {
         await Shell.Current.GoToAsync("//Dashboard");
     }
-
     private async void OnRegisterTapped(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//MainPage");
