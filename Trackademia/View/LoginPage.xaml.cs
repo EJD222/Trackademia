3631489ab2c50058133a21fafc7ce66f0b66aa65
EJD222 <eljdiaz500@gmail.com>
2024-12-14 20:04:41 +0800

    Set up Login Page

---
+++ Trackademia/View/LoginPage.xaml.cs
@@ -0,0 +1,18 @@
+namespace Trackademia.View;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+    private async void OnLoginButtonClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Dashboard");
+    }
+
+    private async void OnRegisterTapped(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+}
