f17831d74629ee2a516d45dd5db0ed1e58c4fbdf
EJD222 <eljdiaz500@gmail.com>
2024-12-14 20:00:26 +0800

    Add UI on Main Page

--- Trackademia/MainPage.xaml.cs
+++ Trackademia/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Trackademia
+﻿using Trackademia.View;
+
+namespace Trackademia
 {
     public partial class MainPage : ContentPage
     {
@@ -10,6 +12,11 @@
         private async void ClickedViewUser(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//UserPage");
+        }
+
+        private async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
         }
     }
 
